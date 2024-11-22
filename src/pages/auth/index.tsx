713c90a304b6eda953fa5fc5cc483f2879f679f7
tubagusdylan <tubagus.dylanr@gmail.com>
2024-11-22 07:11:18 +0700

    fix: profile state

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,17 +1,35 @@
 import { FC, ReactNode } from "react";
 import { useEffect } from "react";
 import { AuthState } from "@services/auth/state";
+import { useRefreshTokenQuery } from "@services/auth/api";
+import { jwtDecode } from "jwt-decode";
 
 interface AuthProviderProps {
   children: ReactNode;
 }
 
+interface JwtPayload {
+  id: string;
+  username: string;
+  profileName: string;
+  isAdmin: string;
+}
+
 const AuthProvider: FC<AuthProviderProps> = ({ children }) => {
   const { setAuthUserState } = AuthState();
+  const { data, isSuccess } = useRefreshTokenQuery();
 
   useEffect(() => {
-    setAuthUserState();
-  }, []);
+    if (isSuccess) {
+      const decoded: JwtPayload = jwtDecode(data?.data.token as string);
+      const payload = {
+        username: decoded.username,
+        profileName: decoded.profileName,
+        isAdmin: decoded.isAdmin,
+      };
+      setAuthUserState(payload);
+    }
+  }, [isSuccess]);
 
   return <>{children}</>;
 };
--- src/services/auth/state.ts
+++ src/services/auth/state.ts
@@ -1,12 +1,10 @@
 import { useCallback } from "react";
-import { jwtDecode } from "jwt-decode";
 import { setAuthUser, setLogoutUser } from "./slice";
 import { useAppDispatch } from "@services/hooks";
-import { useRefreshTokenQuery, useLogoutMutation } from "./api";
+import { useLogoutMutation } from "./api";
 import { toast } from "react-toastify";
 
-interface JwtPayload {
-  id: string;
+interface ParamPayload {
   username: string;
   profileName: string;
   isAdmin: string;
@@ -14,25 +12,10 @@ interface JwtPayload {
 
 export const AuthState = () => {
   const dispatch = useAppDispatch();
-  const { data, isLoading, isError } = useRefreshTokenQuery();
   const [logout] = useLogoutMutation();
 
-  const setAuthUserState = useCallback(() => {
-    if (isError) {
-      return;
-    }
-
-    if (!isLoading && data?.data) {
-      const decoded: JwtPayload = jwtDecode(data?.data.token as string);
-
-      const payload = {
-        username: decoded.username,
-        profileName: decoded.profileName,
-        isAdmin: decoded.isAdmin,
-      };
-
-      dispatch(setAuthUser(payload));
-    }
+  const setAuthUserState = useCallback((payload: ParamPayload) => {
+    dispatch(setAuthUser(payload));
   }, []);
 
   const setLogoutUserState = useCallback(async () => {
